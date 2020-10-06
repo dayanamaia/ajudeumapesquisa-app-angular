b0d782d9ad825c511b90649ba0f8b7a3f044c100
Dayana Maia <dayrmaia@gmail.com>
2020-10-06 16:01:09 -0300

    estrutura antiga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PesquisaComponent } from './pages/pesquisa/pesquisa.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    HomeComponent,
+    PesquisaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
